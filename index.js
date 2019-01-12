65e12844deaeaaf7b5bfbda08517a1376f155507
Yingqi Chen <chanwk18@hotmail.com>
2019-01-12 02:42:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,13 @@ console.log(uppercase)
 function logWhisper(string){
   console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string){
+  if (string='I love you, Grandma.'){
+    return "I love you, too."
+  }else if (string.toUpperCase===string){
+    return "YES INDEED!"
+  }else{
+    return"I can't hear you！"
+  }
+}
